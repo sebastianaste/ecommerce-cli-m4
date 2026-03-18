964fb34b98241bb4cf2f06d74d4a198f5ff4272a
Sebastian Aste <sebastianastebustos@gmail.com>
2026-03-17 23:34:33 -0300

    CartItem and Cart classes implemented.

--- src/main/java/Cart.java
+++ src/main/java/Cart.java
@@ -1,2 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cart {
+    List<CartItem> items = new ArrayList<>();
+
+    public void addItem(Product product, int quantity){
+        for (CartItem item : items){
+            if (item.getProduct().getId() == product.getId()){
+                item.setQuantity(item.getQuantity()+quantity);
+                return;
+            }
+        }
+        items.add(new CartItem(product,quantity));
+    }
+
+    public boolean removeItem(int productId) {
+        for (CartItem item : items) {
+            if (item.getProduct().getId() == productId) {
+                items.remove(item);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<CartItem> getItems(){
+        return items;
+    }
+
+    public boolean isEmpty(){
+        return items.isEmpty();
+    }
+
+    public double getBaseTotal(){
+        double total = 0;
+        for (CartItem item: items){
+            total += item.getSubtotal();
+        }
+        return total;
+    }
+
+    public void clear() {
+        items.clear();
+    }
 }
--- src/main/java/InvalidQuantityException.java
+++ src/main/java/InvalidQuantityException.java
@@ -1,5 +1,5 @@
 public class InvalidQuantityException extends RuntimeException {
-    public InvalidQuantityException(String message) {
-        super(message);
+    public InvalidQuantityException(int quantity) {
+        super("Invalid quantity: " + quantity + ". Should be greater than 0.");
     }
 }
